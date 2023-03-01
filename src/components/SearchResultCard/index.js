4c4b306086093249b1db7876539315904b805ffa
HumbertoBPF <humbertobpf@gmail.com>
2023-02-28 23:19:03 -0300

    :sparkles: Finishing implementing the search tool feature :sparkles:

--- src/components/SearchResultCard/index.js
+++ src/components/SearchResultCard/index.js
@@ -1,12 +1,17 @@
+import styles from "./SearchResultCard.module.css";
+
 export default function SearchResultCard({ word, language, flagImage }) {
     return (
         <div className="col-6 col-sm-4 my-1">
             <a className="text-reset text-decoration-none" href="#">
-                <div className="card secondary-color-card search-card">
+                <div className={`card ${styles["search-card"]}`}>
                     <div className="row g-0">
                         <div className="col-md-4 px-4 d-flex align-items-center justify-content-center">
                             <div>
-                                <img src={flagImage} className="img-fluid rounded-start" alt={`${language} language flag`}/>
+                                <img 
+                                    src={`data:image/jpeg;base64,${flagImage}`} 
+                                    className="img-fluid rounded-start" 
+                                    alt={`${language} language flag`}/>
                             </div>
                         </div>
                         <div className="col-md-8 d-flex align-items-center justify-content-center">
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,3 +1,4 @@
+import Pagination from "components/Pagination";
 import SearchResultCard from "components/SearchResultCard";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,24 +9,51 @@ export default function SearchResults() {
     const [searchParams] = useSearchParams();
     const dispatch = useDispatch();
     const languages = useSelector(state => state.languages);
-    const [searchResults, setSearchResults] = useState([]);
+    const [searchResults, setSearchResults] = useState({results: []});
+    const [languagesFlag, setLanguagesFlag] = useState(new Map());
 
     useEffect(() => {
         dispatch(fetchLanguages());
     }, [dispatch]);
 
     useEffect(() => {
+        // Preparing language map
+        let temp = languagesFlag;
+        temp.clear();
+        languages.forEach(item => {
+            temp.set(item.language_name, item.flag_image);
+        });
+        setLanguagesFlag(temp);
+        // Fetching results
         const url = `http://localhost:8000/api/search?${searchParams}`;
-        fetch(url).then((response) => response.json()).then((data) => setSearchResults(data.results));
+        fetch(url).then((response) => response.json()).then((data) => {
+            data.page = 1;
+            setSearchResults(data);
+        });
     }, [languages]);
 
     return (
-        <div className="row justify-content-center">
-            { searchResults.map((item) => <SearchResultCard 
-                                            key={item.id} 
-                                            word={item.word_name} 
-                                            language={item.language} 
-                                            flagImage=""/>) }
-        </div>
+        <>
+            <div className="row justify-content-center mb-4">
+                { searchResults.results.map((item) => <SearchResultCard 
+                                                key={item.id} 
+                                                word={item.word_name} 
+                                                language={item.language} 
+                                                flagImage={languagesFlag.get(item.language)}/>) }
+            </div>
+            <Pagination 
+                    previous={searchResults.previous} 
+                    next={searchResults.next} 
+                    count={searchResults.count} 
+                    resultsPerPage={12} 
+                    page={searchResults.page} 
+                    callback={(page) => {
+                        const url = `http://localhost:8000/api/search?${searchParams}&page=${page}`;
+                        fetch(url).then((response) => response.json()).then((data) => {
+                            data.page = page;
+                            setSearchResults(data);
+                        });
+                    }}/>
+        </>
     );
 }
