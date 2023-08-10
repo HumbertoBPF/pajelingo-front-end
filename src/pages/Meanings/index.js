6829e87366cbcf8cba6d6f81d1f92901e3d0bc1e
HumbertoBPF <humbertobpf@gmail.com>
2023-08-10 08:37:14 -0300

    :children_crossing: Displaying spinner while meaning  page loads :children_crossing:

--- src/pages/Meanings/index.js
+++ src/pages/Meanings/index.js
@@ -5,12 +5,13 @@ import { useParams } from "react-router-dom";
 import CustomButton from "components/CustomButton";
 import HeartIcon from "components/icons/HeartIcon";
 import { getMeaning, getWord, toggleFavoriteWord } from "api/words";
+import CustomSpinner from "components/CustomSpinner";
 
 export default function Meanings() {
   const params = useParams();
   const user = useSelector((state) => state.user);
-  const [meanings, setMeanings] = useState([]);
-  const [word, setWord] = useState({ word_name: "" });
+  const [meanings, setMeanings] = useState(undefined);
+  const [word, setWord] = useState(undefined);
 
   useEffect(() => {
     // Fetching word
@@ -46,41 +47,59 @@ export default function Meanings() {
     );
   }
 
+  function renderMeanings() {
+    return (
+      <>
+        <h5 className="mb-4" data-testid="title">
+          Meanings of &quot;{word.word_name}&quot;
+        </h5>
+
+        {word.image ? (
+          <div className="row g-0 justify-content-center mb-4">
+            <img
+              src={`data:image/jpeg;base64,${word.image}`}
+              className="img-fluid rounded col-md-2 col-sm-3 col-6"
+              alt="Word meaning"
+              data-testid="word-image"
+            />
+          </div>
+        ) : null}
+
+        {meanings.length > 1
+          ? meanings.map((meaning, index) => (
+              <MeaningCard
+                key={meaning.id}
+                index={index + 1}
+                meaning={meaning}
+              />
+            ))
+          : meanings.map((meaning) => (
+              <MeaningCard key={meaning.id} meaning={meaning} />
+            ))}
+
+        {user ? (
+          <div className="text-center">
+            <CustomButton
+              variant="info"
+              onClick={() => toggleFavoriteButton()}
+              testId="favorite-button">
+              {renderFavoriteButton(word)}
+            </CustomButton>
+          </div>
+        ) : null}
+      </>
+    );
+  }
+
   return (
     <>
-      <h5 className="mb-4" data-testid="title">
-        Meanings of &quot;{word.word_name}&quot;
-      </h5>
-
-      {word.image ? (
-        <div className="row g-0 justify-content-center mb-4">
-          <img
-            src={`data:image/jpeg;base64,${word.image}`}
-            className="img-fluid rounded col-md-2 col-sm-3 col-6"
-            alt="Word meaning"
-            data-testid="word-image"
-          />
-        </div>
-      ) : null}
-
-      {meanings.length > 1
-        ? meanings.map((meaning, index) => (
-            <MeaningCard key={meaning.id} index={index + 1} meaning={meaning} />
-          ))
-        : meanings.map((meaning) => (
-            <MeaningCard key={meaning.id} meaning={meaning} />
-          ))}
-
-      {user ? (
+      {meanings && word ? (
+        renderMeanings()
+      ) : (
         <div className="text-center">
-          <CustomButton
-            variant="info"
-            onClick={() => toggleFavoriteButton()}
-            testId="favorite-button">
-            {renderFavoriteButton(word)}
-          </CustomButton>
+          <CustomSpinner />
         </div>
-      ) : null}
+      )}
     </>
   );
 }
